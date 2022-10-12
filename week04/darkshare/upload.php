6c0ebf8af940bbfeeff2efba3cc0dad0a70c2e5d
dieterdeweirdt <dieter.deweirdt@arteveldehs.be>
2022-10-12 14:25:56 +0200

    updates

--- week04/darkshare/upload.php
+++ week04/darkshare/upload.php
@@ -1,9 +1,26 @@
 <?php
 require_once 'config.php';
 
+$error = '';
+
 //create upload_dir if it doesn't exist
+if ( ! is_dir(UPLOAD_PATH) ) {
+    mkdir(UPLOAD_PATH);
+}
 
 //upload the file + check the MIME type
+//print_r($_FILES);
+
+$from = $_FILES['my_file']['tmp_name'];
+$mime_type = $_FILES['my_file']['type'];
+$to = UPLOAD_PATH . '/' . $_FILES['my_file']['name'];
+
+if( in_array($mime_type, SAFE_CONTENT_TYPES) ) {
+    move_uploaded_file($from, $to);
+} else {
+    $error = 'Bestandstype niet toegestaan!';
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,7 +36,7 @@ require_once 'config.php';
 <h1>DarkShare</h1>
 
 <section>
-Foutmelding hier
+<?= $error; ?>
 </section>
 
 </body>
