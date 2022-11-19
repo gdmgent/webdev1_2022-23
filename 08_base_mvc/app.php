37c58db42c02580bf1a3086c5d3116be9190d6a0
dieterdeweirdt <dieter.deweirdt@arteveldehs.be>
2022-11-19 17:51:19 +0100

    updates

--- 08_base_mvc/app.php
+++ 08_base_mvc/app.php
@@ -12,11 +12,11 @@ session_start();
 $db = new PDO($config['db_connection'] . ':dbname=' . $config['db_database'] . ';host=' . $config['db_host'] . ';port=' . $config['db_port'], $config['db_username'], $config['db_password']);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
-//router aanmaken
+//routes aanmaken
 $router = new \Bramus\Router\Router();
-
-$router->get('/', function() { echo 'Index'; });
-$router->get('/cocktails', 'App\Controllers\CocktailController@index');
+//$router->get('/', function() { echo 'Index'; });
+$router->get('/', 'App\Controllers\CocktailController@index');
 $router->get('/cocktail/(\d+)', 'App\Controllers\CocktailController@detail');
 
+//Run
 $router->run();
